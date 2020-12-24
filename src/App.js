6d7d193bae0442137841bac504373e00b36bfb1c
Ariel Walley <arielwalley95@gmail.com>
2020-12-24 12:52:52 -0500

    Installed styled-components, added header

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Body = styled.div`
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+`;
+
+const Header = styled.h1`
+  margin: 0 auto;
+  padding: 15px;
+  background-color: #0F52BA;
+  color: white;
+  font-size: 40px;
+  font-weight: 700;
+  text-align: center;  
+`;
+
+class App extends React.Component {
+
+  render() {
+    return(
+      <Body>
+        <Header>GPA Calculator</Header>
+      </Body>
+    )
+  }
+
 }
 
 export default App;
+

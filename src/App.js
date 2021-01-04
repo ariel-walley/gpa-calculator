78247919a2963b53c07f965d87ba2d1ac35e9fd2
Ariel Walley <arielwalley95@gmail.com>
2021-01-04 04:28:04 -0500

    Bug fixes and minor formatting changes
    - Ensured each component had margin and padding specified
    - Realigned header to be aligned with rows
    - Call calculateGPA after a row is deleted
    - Not update GPA if "NaN" when GPA points are divded by zero

--- src/App.js
+++ src/App.js
@@ -35,14 +35,17 @@ const CalcContainer = styled.div`
 /* Calculator Header */
 
 const TableHeader = styled.div`
-  width: 100%;
+  margin: 0;
+  padding: 0 10px;
   background-color: #0F52BA;
   display: flex;
-  justify-content: space-around;
+  justify-content: center;
   align-items: center;
 `;
 
 const HeaderText = styled.h1`
+  margin: 5px 7px;
+  padding: 0;
   color: white;
   font-size: 17px;
   font-weight: 700;
@@ -61,10 +64,16 @@ const GradeHeader = styled(HeaderText)`
   width: 90px;
 `;
 
+const SpacerHeader = styled(HeaderText)`
+  width: 18px;
+`;
+
 /* Calculator Row */
 
 const Row = styled.div`
   height: 40px;
+  margin: 0;
+  padding: 0 10px;
   background-color: lightgrey;
   display: flex;
   flex-wrap: no-wrap;
@@ -73,11 +82,12 @@ const Row = styled.div`
 `;
 
 const Input = styled.input`
-  font-size: 17px;
-  text-align: center;
-  margin: 5px;
+  margin: 0 5px;
+  padding: 0px;
   border: 2px solid darkgray;
   border-radius: 5px;
+  font-size: 17px;
+  text-align: center;
 `;
 
 const CourseInput = styled(Input)`
@@ -96,22 +106,25 @@ const GradeInput = styled(Input)`
 
 const AddRow = styled.div`
   background-color: lightgrey;
-  padding: 10px 0;
+  margin: 0;
+  padding: 7px 0;
   text-align: center;
   font-size: 14px;
   font-weight: 700;
 `;
 
 const RemoveRow = styled.img`
+  margin: 0 5px;
+  padding: 0;
   width: 20px;
   height: 20px;
-  margin-right: 5px;
 `;
 
 /* Calculator Total Row */
 
 const TotalRow = styled.div`
   background-color: darkgray;
+  margin: 0;
   padding: 10px 0;
   text-align: center;
   font-weight: 700;
@@ -177,6 +190,7 @@ class App extends React.Component {
     this.setState({
       rows: currentRows
     })
+    this.calculateGPA();
   }
  
   calculateGPA() {
@@ -220,9 +234,11 @@ class App extends React.Component {
     
     let newGPA = (gpaPoints/creditSum).toFixed(2);
     
-    this.setState({
-      gpa: newGPA
-    })
+    if (newGPA !== 'NaN') {
+      this.setState({
+        gpa: newGPA
+      })
+    }
   }
 
   render() {
@@ -234,6 +250,7 @@ class App extends React.Component {
             <CourseHeader>Course</CourseHeader>
             <CreditHeader>Credit Hours</CreditHeader>
             <GradeHeader>Grade</GradeHeader>
+            <SpacerHeader/>
           </TableHeader>
           {this.renderRows()}
           <AddRow onClick={this.addRow}>+ Add Row</AddRow>
